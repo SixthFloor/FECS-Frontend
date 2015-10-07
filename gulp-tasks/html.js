83dacc5caaeca1b9c0606fd1fc6d9a9f69612fbd
ChinCluBi <chincub@gmail.com>
2015-10-07 10:43:50 +0700

    Fix gulp

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var browserSync = require('browser-sync')
 
 gulp.task('html', function () {
-  return gulp.src('./src/**/*.html')
+  return gulp.src(['./src/**/*.html', './src/**/*.tmpl'])
     .pipe(gulp.dest('./www/'))
     .pipe(browserSync.reload({
       stream: true
