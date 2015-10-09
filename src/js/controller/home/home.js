738d1b6f3528049869eae16c305ba8d90745c24c
GuroKung <greenblood11@hotmail.com>
2015-10-09 10:36:14 +0700

    remove unuse service

--- src/js/controller/home/home.js
+++ src/js/controller/home/home.js
@@ -10,47 +10,6 @@
     .module('controller.homepage', [])
     .controller('HomePageController', HomePageController)
     .controller('LoginController', LoginController)
-    .service('FECSAuth', FECSAuth)
-
-  FECSAuth.$inject = ['$window']
-  function FECSAuth($window) {
-  	var self = this
-  	
-  	self.test = "Hello";
-
-  	self.isAuthed = function() {
-     if (self.getToken()) return true;
-   	 else return false;		
-	}
-
-  	self.setToken = function(token) {
-	  $window.localStorage['authToken'] = token;
-	}
-
-	self.getToken = function() {
-	  var token = $window.localStorage['authToken'];
-	  return token ? token : false;
-	}
-
-    self.login = function(email, pwd) {
-    	console.log("email: "+ email + "  password: " + pwd)
-	    //for real use
-	     //  return $http.post('waitForSomeAPI', {
-		    //   email: email,
-		    //   password: pwd
-	   	 // })
-		
-		// if success
-		var fakeToken = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		self.setToken(fakeToken)
-		console.log("Token: " + self.getToken())
-  	}
-
-  	self.logout = function() {
-	  $window.localStorage.removeItem('authToken');
-	}
-
-	}
 
   HomePageController.$inject = ['$scope']
   function HomePageController ($scope) {
@@ -70,7 +29,6 @@
     self.login = function(){
     	FECSAuth.login(self.email,self.pwd)
     }
-
-
+    
   }
 })()
