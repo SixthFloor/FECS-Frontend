3609a63ab4fb251a9fce0d05918631769c39a230
Waranyu Rerkdee <waranyu.1993@gmail.com>
2015-10-12 04:18:09 +0700

    handle product not found

--- src/js/controller/home/product.js
+++ src/js/controller/home/product.js
@@ -16,39 +16,46 @@
     // path of mock API
     var url = 'http://128.199.112.126:3000/product/' + $stateParams.product_id
     $http.get(url).success(function (response) {
-      console.log(response)
-      console.log(response.data.name)
-      console.log($stateParams.product_id)
+      if (response.status !== 'error') {
+        console.log(response)
+        console.log(response.data.name)
+        console.log($stateParams.product_id)
 
-      self.productName = angular.uppercase(response.data.name)
-      self.productID = angular.uppercase(response.data.serialNumber)
-      self.property1 = {
-        repeatSelect: null,
-        availableOptions: [
-          {id: 'green', name: 'Green'},
-          {id: 'white', name: 'White'},
-          {id: 'black', name: 'Black'}
-        ]
+        self.productName = angular.uppercase(response.data.name)
+        self.productID = angular.uppercase(response.data.serialNumber)
+        self.property1 = {
+          repeatSelect: null,
+          availableOptions: [
+            {id: 'green', name: 'Green'},
+            {id: 'white', name: 'White'},
+            {id: 'black', name: 'Black'}
+          ]
+        }
+
+        self.property2 = {
+          repeatSelect: null,
+          availableOptions: [
+            {id: 'size30', name: 'Size-30'},
+            {id: 'size32', name: 'Size-32'},
+            {id: 'size34', name: 'Size-34'}
+          ]
+        }
+
+        self.forceUnknownOption = function () {
+          self.property1.repeatSelect = 'error'
+          self.property2.repeatSelect = 'error'
+        }
+
+        self.available = 5
+        self.price = response.data.price
+        self.description = response.data.description
+        self.category = angular.uppercase(response.data.category.name)
+      } else {
+        console.log(response.message)
+        self.is404 = true
+        document.querySelector('title').innerHTML = response.message
+        self.errorMessage = 'Error: ' + response.message
       }
-
-      self.property2 = {
-        repeatSelect: null,
-        availableOptions: [
-          {id: 'size30', name: 'Size-30'},
-          {id: 'size32', name: 'Size-32'},
-          {id: 'size34', name: 'Size-34'}
-        ]
-      }
-
-      self.forceUnknownOption = function () {
-        self.property1.repeatSelect = 'error'
-        self.property2.repeatSelect = 'error'
-      }
-
-      self.available = 5
-      self.price = response.data.price
-      self.description = response.data.description
-      self.category = angular.uppercase(response.data.category.name)
     })
   }
 })()
