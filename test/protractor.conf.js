050cc27a897a2970ec325acf8d18c71412ec03f8
ChinCluBi <chincub@gmail.com>
2015-10-12 21:58:00 +0700

    Add test report

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,3 +1,5 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter')
+
 exports.config = {
   allScriptsTimeout: 4000,
 
@@ -13,5 +15,15 @@ exports.config = {
 
   jasmineNodeOpts: {
     defaultTimeoutInterval: 4000
+  },
+
+  onPrepare: function () {
+    jasmine.getEnv().addReporter(
+      new Jasmine2HtmlReporter({
+        savePath: './test/reports/',
+        screenshotsFolder: 'images',
+        takeScreenshotsOnlyOnFailures: true
+      })
+    )
   }
 }
