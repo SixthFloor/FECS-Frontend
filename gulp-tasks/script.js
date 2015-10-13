ff52019d67d10978a951ab311c42a90f3da63af7
ChinCluBi <chincub@gmail.com>
2015-10-14 01:38:42 +0700

    New login page and complete connect to API

--- gulp-tasks/script.js
+++ gulp-tasks/script.js
@@ -8,6 +8,7 @@ gulp.task('script', function () {
     './bower_components/jquery/dist/jquery.js',
     './bower_components/angular/angular.js',
     './bower_components/angular-ui-router/release/angular-ui-router.js',
+    './bower_components/angular-notify/dist/angular-notify.js',
     './bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
     './src/js/**/*.js'])
     .pipe(concat('scripts.js'))
