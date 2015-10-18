acd8d0fe692b0faad5f180ac15f3e4c1914f2764
JL <world.jadi@gmail.com>
2015-10-18 19:17:48 +0700

    unsuccess remove notify

--- test/protractor_test/testLogin.js
+++ test/protractor_test/testLogin.js
@@ -14,8 +14,8 @@ describe('sendKey to login', function() {
   var loginButton = element(by.buttonText('Sign in'))
   var afterLogin = element(by.css('[ng-if="accessToken != false"]'))
   var closeHiden = element(by.css('[aria-hidden]="true"'))
-  var notify = element(by.className('cg-notify-message ng-scope alert alert-success cg-notify-message-center'))
-  
+  var notify = $('.cg-notify-message')
+  var notifyButton = $('.cg-notify-close')
 
   function Login(a,b) {
     //browser.get('http://localhost:3030/#');
@@ -25,31 +25,36 @@ describe('sendKey to login', function() {
 
   function testCases() {
     case1()
-    case2()
+    // case2()
   }
 
   function case1(){
-    linkSignin.click()
+    browser.isElementPresent(linkSignin).then(linkSignin.click())
+    // linkSignin.click()
     Login('nititest@hotmail.com','nititest')
     loginButton.click()
-    browser.sleep(10000);
-    browser.waitForAngular()
+    browser.sleep(2500)
+    browser.driver.executeScript("$('.cg-notify-message').remove();")
+    
+    
+    
     //browser.ignoreSynchronization = true
     //expect(notify.getText()).toEqual('Sign out')
     //browser.ignoreSynchronization = false
-    linkSignout.click()
+    browser.isElementPresent(linkSignout).then(linkSignout.click())
+    // linkSignout.click()
   }
 
-  function case2(){
-    linkSignin.click()
-    Login('nititest2@hotmail.com','nititest')
-    loginButton.click()
-    browser.sleep(10000);
-    browser.waitForAngular()
-    //closeHiden.click()
-    //expect(afterLogin.getText()).toEqual('Welcome to the FECS Sign out')
-    //linkSignout.click()
-  }
+  // function case2(){
+  //   linkSignin.click()
+  //   Login('nititest2@hotmail.com','nititest')
+  //   loginButton.click()
+  //   browser.sleep(10000)
+  //   browser.waitForAngular()
+  //   //closeHiden.click()
+  //   //expect(afterLogin.getText()).toEqual('Welcome to the FECS Sign out')
+  //   //linkSignout.click()
+  // }
 
   function case3(){
     
