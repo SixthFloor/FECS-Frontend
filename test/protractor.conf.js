12f565ebd9bd51acb3650aa29bc3a3e2295fd92b
Kittinan <kitty_npp@hotmail.com>
2015-10-19 22:12:23 +0700

    fix test regiter bug

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,12 +1,11 @@
 //var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter')
-var HtmlReporter = require('protractor-html-screenshot-reporter')
+//var HtmlReporter = require('protractor-html-screenshot-reporter')
 
 exports.config = {
-  allScriptsTimeout: 4000,
+  // allScriptsTimeout: 4000,
 
 
-  specs: ['protractor_test/test_LoginAsMember.js', 
-         'protractor_test/test_RegisterAsMember.js'],
+  specs: ['protractor_test/test_RegisterAsMember.js'],
 
   capabilities: {
     'browserName': 'chrome',
@@ -15,15 +14,15 @@ exports.config = {
   baseUrl: 'http://localhost:8080/app/',
 
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 60000
+    // defaultTimeoutInterval: 60000
   },
 
-  onPrepare: function () {
-    jasmine.getEnv().addReporter(new HtmlReporter({
-         baseDirectory: 'test_reports/',
-         takeScreenshotsOnlyOnFailures: true,
-         docTitle: 'Test Beta',
-         docName: 'test_beta.html'
-      }))
-  }
+  // onPrepare: function () {
+  //   jasmine.getEnv().addReporter(new HtmlReporter({
+  //        baseDirectory: 'test_reports/',
+  //        takeScreenshotsOnlyOnFailures: true,
+  //        docTitle: 'Test Beta',
+  //        docName: 'test_beta.html'
+  //     }))
+  // }
 }
--- test/protractor_test/test_RegisterAsMember.js
+++ test/protractor_test/test_RegisterAsMember.js
@@ -34,8 +34,15 @@ describe('register as member', function () {
         browser.sleep(1000)
     })
 
+    it('Case 0: If click registerButton without any information,register should not be successed', function () {
+        registerButton.click()
+        expect(browser.getCurrentUrl()).toBe('http://localhost:3030/#/register')
+        enterResgiterPage()
+        browser.sleep(2500)
+    })
+
     it('Case 1: If e-mail,password,name and lastname are corrected, register should be successed and access to login page', function () {
-        email.sendKeys("newped111@gmail.com")
+        email.sendKeys("newped1234@gmail.com")
         password.sendKeys("12345678")
         confirmpassword.sendKeys("12345678")
         firstname.sendKeys("ped")
@@ -50,7 +57,7 @@ describe('register as member', function () {
 
     it('Case 2: If register with same e-mail, notification should appear register unsuccessful', function () {
         clearInformation()
-        email.sendKeys("newped111@gmail.com")
+        email.sendKeys("newped1234@gmail.com")
         password.sendKeys("12345678")
         confirmpassword.sendKeys("12345678")
         firstname.sendKeys("ped")
@@ -62,27 +69,28 @@ describe('register as member', function () {
         browser.sleep(2500)
     })
 
-    it('Case 3: If register with password longer than 20 letters, notification should appear register unsuccessful', function () {
+    it('Case 3: If register without lastname, notification should appear register unsuccessful', function () {
         clearInformation()
-        email.sendKeys("newnewped112@gmail.com")
-        password.sendKeys("1234567891011121314151617181920")
-        confirmpassword.sendKeys("1234567891011121314151617181920")
+        email.sendKeys("newnewped117@gmail.com")
+        password.sendKeys("12345678")
+        confirmpassword.sendKeys("12345678")
         firstname.sendKeys("ped")
-        lastname.sendKeys("noi")
-        expect(element(by.css('.alert.alert-danger.col-sm-4.col-sm-offset-2')).isDisplayed()).toBeTruthy()
+        registerButton.click()
         browser.sleep(1000)
+        expect(browser.getCurrentUrl()).toBe('http://localhost:3030/#/register')
         enterResgiterPage()
         browser.sleep(2500)
     })
 
     it('Case 4: If register with password shorter than 8 letters, notification should appear register unsuccessful', function () {
         clearInformation()
-        email.sendKeys("newnewped112@gmail.com")
+        email.sendKeys("newnewped117@gmail.com")
         password.sendKeys("123")
         confirmpassword.sendKeys("123")
         firstname.sendKeys("ped")
         lastname.sendKeys("noi")
-        expect(element(by.css('.alert.alert-danger.col-sm-4.col-sm-offset-2')).isDisplayed()).toBeTruthy()
+        registerButton.click()
+        expect(browser.getCurrentUrl()).toBe('http://localhost:3030/#/register')
         browser.sleep(1000)
         enterResgiterPage()
         browser.sleep(2500)
@@ -90,20 +98,21 @@ describe('register as member', function () {
 
     it('Case 5: If register email with incorrect form, notification should appear register unsuccessful', function () {
         clearInformation()
-        email.sendKeys("newnewped112-gmail.com")
+        email.sendKeys("newnewped117-gmail.com")
         password.sendKeys("1235678")
         confirmpassword.sendKeys("1235678")
         firstname.sendKeys("ped")
         lastname.sendKeys("noi")
-        expect(element(by.css('.alert.alert-danger.col-sm-4.col-sm-offset-2')).isDisplayed()).toBeTruthy()
+        registerButton.click()
         browser.sleep(1000)
+        expect(browser.getCurrentUrl()).toBe('http://localhost:3030/#/register')
         enterResgiterPage()
         browser.sleep(2500)
     })
 
     it('Case 6: If register password that does not match, notification should appear register unsuccessful', function () {
         clearInformation()
-        email.sendKeys("newnewped112@gmail.com")
+        email.sendKeys("newnewped117@gmail.com")
         password.sendKeys("12345678")
         confirmpassword.sendKeys("87654321")
         firstname.sendKeys("ped")
@@ -116,9 +125,9 @@ describe('register as member', function () {
 
     it('Case 7: If register password with special charactor, notification should appear register unsuccessful', function () {
         clearInformation()
-        email.sendKeys("newnewped112@gmail.com")
-        password.sendKeys("$$$$$$$$$$")
-        confirmpassword.sendKeys("$$$$$$$$$$")
+        email.sendKeys("newnewped117@gmail.com")
+        password.sendKeys("$&#!$&#!")
+        confirmpassword.sendKeys("$&#!$&#!")
         firstname.sendKeys("ped")
         lastname.sendKeys("noi")
         expect(element(by.css('.alert.alert-danger.col-sm-4.col-sm-offset-2')).isDisplayed()).toBeTruthy()
@@ -129,7 +138,7 @@ describe('register as member', function () {
 
     it('Case 8: If register with all correct information, notification should appear successed and change to login page', function () {
         clearInformation()
-        email.sendKeys("newnewped112@gmail.com")
+        email.sendKeys("newnewped1234@gmail.com")
         password.sendKeys("12345678")
         confirmpassword.sendKeys("12345678")
         firstname.sendKeys("ped")
