caa34aff04accd76152d9c5b27588782490fea29
GuroKung <greenblood11@hotmail.com>
2015-10-20 18:05:17 +0700

    add email not valid alert

--- src/js/controller/register.js
+++ src/js/controller/register.js
@@ -18,7 +18,6 @@
 
     self.checkEmail = function validateEmail (email) {
       var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
-      console.log(re.test(email))
       return re.test(email)
     }
 
