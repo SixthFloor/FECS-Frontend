7d0cc92f60363b4b505d2dd624c2740e7e2fead2
JL <world.jadi@gmail.com>
2015-11-14 21:04:20 +0700

    fix yml to use script

---
+++ test_script.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -ex
+
+REPORT_DATE="$(date +"%m-%d-%y")"
+
+TEST_TIME="$(date +"%H:%M:%S")"
+
+npm run update-webdriver
+gulp compile
+http-server ./www -p 3030 &WEBSERVER_PID=$!
+echo '$1 = ' $1
+if [ $1 = "-protractor" ];
+then
+	protractor test/protractor.conf.js && echo "skip"
+	mv ./reports/ test_"${REPORT_DATE}"_"(${NUMBER_FILE})"/
+	sshpass -p jl scp -r test_"${REPORT_DATE}"_"(${TEST_TIME})"/ jl@128.199.133.224:/home/jl/Test_reports/feature-tests/ -o StrictHostKeyChecking=no
+elif [ "$1" = "-unit" ];
+then
+	npm run unit
+else
+	npm run runtest
+fi
+
+killall node
