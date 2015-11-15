7d5da85469d90f66b2fb4444ad14076a084ae507
JL <world.jadi@gmail.com>
2015-11-15 07:19:37 +0700

    full path

--- test_script.sh
+++ test_script.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+# /home/travis/build/SixthFloor/FECS-Web/test/protractor_test/test_LoginAsMember.js
+
 REPORT_DATE="$(date +"%m-%d-%y")"
 
 TEST_TIME="$(date +"%H:%M:%S")"
@@ -13,7 +15,7 @@ if [ $1 = "-protractor" ];
 then
 	protractor test/protractor.conf.js && echo "skip"
 	mv ./reports/ test_"${REPORT_DATE}"_"(${TEST_TIME})"/
-	sshpass -p jl scp -o StrictHostKeyChecking=no -pr -P 22 test_"${REPORT_DATE}"_"(${TEST_TIME})" jl@128.199.133.224:/home/jl/Test_reports/feature-tests/
+	sshpass -p jl scp -o StrictHostKeyChecking=no -pr -P 22 /home/travis/build/SixthFloor/FECS-Web/test_"${REPORT_DATE}"_"(${TEST_TIME})" jl@128.199.133.224:/home/jl/Test_reports/feature-tests/
 elif [ "$1" = "-unit" ];
 then
 	npm run unit
