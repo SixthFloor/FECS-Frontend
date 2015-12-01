21a09e58cc35e60ff63f33593fbf652b6ad40ee1
JL <world.jadi@gmail.com>
2015-12-01 18:20:09 +0700

    fix test script and continue ViewFurnitureCategory

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -2,15 +2,7 @@ var HtmlReporter = require('protractor-html-screenshot-reporter')
 
 exports.config = {
 
-<<<<<<< HEAD
-  specs: ['protractor_test/test_SortFurniture.js'],
-=======
-<<<<<<< HEAD
-  specs: ['protractor_test/test_RegisterAsMember.js'],
-=======
   specs: ['protractor_test/test_ViewFurniture.js'],
->>>>>>> a3959e37da49e21f4f82551bcb28a562b5765244
->>>>>>> 27e0f66aaf8087a956490d01407a894538958647
 
   multiCapabilities: [{
       // browserName: 'firefox'
@@ -25,13 +17,13 @@ exports.config = {
     defaultTimeoutInterval: 180000
   },
 
-  // onPrepare: function () {
-  //   jasmine.getEnv().addReporter(new HtmlReporter({
-  //        baseDirectory: 'reports/',
-  //        takeScreenShotsOnlyForFailedSpecs: true,
-  //        docTitle: 'Test Report',
-  //        preserveDirectory: true,
-  //        docName: 'report.html'
-  //     }))
-  // }
+  onPrepare: function () {
+    jasmine.getEnv().addReporter(new HtmlReporter({
+         baseDirectory: 'reports/',
+         takeScreenShotsOnlyForFailedSpecs: true,
+         docTitle: 'Test Report',
+         preserveDirectory: true,
+         docName: 'report.html'
+      }))
+  }
 }
