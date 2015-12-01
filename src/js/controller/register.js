c64c7ee575c37e753a1bb369af6cbbbad065ae11
ChinCluBi <chincub@gmail.com>
2015-12-01 14:52:00 +0700

    Complete register interface

--- src/js/controller/register.js
+++ src/js/controller/register.js
@@ -21,6 +21,10 @@
     self.moveElement = $('.timeline>dl')
     self.height = self.moveElement.height() + 40
 
+    self.moveElement.resize(function () {
+      self.height = self.moveElement.height() + 40
+    })
+
     self.checkEmail = function validateEmail (email) {
       var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
       return re.test(email)
@@ -50,67 +54,71 @@
       self.steps = registerService.steps
     }
     self.next1 = function () {
-      if ((self.member.confirmpassword === self.member.password) &&
-        (self.member.password.length >= 8 && self.member.confirmpassword.length >= 8) &&
-        (self.checkEmail(self.member.email))) {
-        registerService.valid.step1 = true
-        registerService.steps.step1 = false
-        registerService.steps.step2 = true
-        registerService.steps.step3 = false
-        console.log('Step1 next')
-        self.moveElement.css('margin-top', '-' + self.height + 'px')
-      } else {
-        console.log('form not valid')
-        registerService.valid.step1 = false
-      }
-      self.valid = registerService.valid
-      self.steps = registerService.steps
+      // if ((self.member.confirmpassword === self.member.password) &&
+      //   (self.member.password.length >= 8 && self.member.confirmpassword.length >= 8) &&
+      //   (self.checkEmail(self.member.email))) {
+      //   registerService.valid.step1 = true
+      //   registerService.steps.step1 = false
+      //   registerService.steps.step2 = true
+      //   registerService.steps.step3 = false
+      //   console.log('Step1 next')
+      //   self.moveElement.css('margin-top', '-' + self.height + 'px')
+      // } else {
+      //   console.log('form not valid')
+      //   registerService.valid.step1 = false
+      // }
+      // self.valid = registerService.valid
+      // self.steps = registerService.steps
+      self.moveElement.css('margin-top', '-' + self.height + 'px')
     }
     self.next2 = function () {
-      if ((self.member.firstname !== '') && (self.member.lastname !== '')) {
-        registerService.valid.step2 = true
-        registerService.steps.step1 = false
-        registerService.steps.step2 = false
-        registerService.steps.step3 = true
-        console.log('Step2 next')
-        self.moveElement.css('margin-top', '-' + (self.height * 2) + 'px')
-      } else {
-        console.log('form not valid')
-        registerService.valid.step2 = false
-      }
-      self.valid = registerService.valid
-      self.steps = registerService.steps
+      // if ((self.member.firstname !== '') && (self.member.lastname !== '')) {
+      //   registerService.valid.step2 = true
+      //   registerService.steps.step1 = false
+      //   registerService.steps.step2 = false
+      //   registerService.steps.step3 = true
+      //   console.log('Step2 next')
+      //   self.moveElement.css('margin-top', '-' + (self.height * 2) + 'px')
+      // } else {
+      //   console.log('form not valid')
+      //   registerService.valid.step2 = false
+      // }
+      // self.valid = registerService.valid
+      // self.steps = registerService.steps
+      self.moveElement.css('margin-top', '-' + (self.height * 2) + 'px')
     }
     self.submit = function () {
-      if ((self.member.confirmpassword === self.member.password) &&
-        (self.member.password.length >= 8 && self.member.confirmpassword.length >= 8) &&
-        (self.checkEmail(self.member.email)) &&
-        (self.member.firstname !== '') && (self.member.lastname !== '')) {
-        registerService.regis(function (response) {
-          console.log('Submit regis')
-          if (response.status === 'error') {
-            var msg = '<span><b>Registration not possible </b> ' + response.message + '.</span>'
-            notification.error({
-              message: msg,
-              replaceMessage: true
-            })
-          } else {
-            msg = '<span><b>Success!</b> Welcome ' + self.member.firstname + ' to FECS. <br/> Please Login to the system.</span>'
-            notification.success({
-              message: msg
-            })
-            $state.transitionTo('login')
-          }
-        }, function (response) {
-          console.log(response)
-        })
-      } else {
-        var msg = '<span><b>Form is invalid.</b><br/>Please back to the previous steps.</span>'
-        notification.error({
-          message: msg,
-          replaceMessage: true
-        })
-      }
+      //   if ((self.member.confirmpassword === self.member.password) &&
+      //     (self.member.password.length >= 8 && self.member.confirmpassword.length >= 8) &&
+      //     (self.checkEmail(self.member.email)) &&
+      //     (self.member.firstname !== '') && (self.member.lastname !== '')) {
+      //     registerService.regis(function (response) {
+      //       console.log('Submit regis')
+      //       if (response.status === 'error') {
+      //         var msg = '<span><b>Registration not possible </b> ' + response.message + '.</span>'
+      //         notification.error({
+      //           message: msg,
+      //           replaceMessage: true
+      //         })
+      //       } else {
+      //         // msg = '<span><b>Success!</b> Welcome ' + self.member.firstname + ' to FECS. <br/> Please Login to the system.</span>'
+      //         // notification.success({
+      //         //   message: msg
+      //         // })
+      //         // $state.transitionTo('login')
+      //         self.moveElement.css('margin-top', '-' + (self.height * 3) + 'px')
+      //       }
+      //     }, function (response) {
+      //       console.log(response)
+      //     })
+      //   } else {
+      //     var msg = '<span><b>Form is invalid.</b><br/>Please back to the previous steps.</span>'
+      //     notification.error({
+      //       message: msg,
+      //       replaceMessage: true
+      //     })
+      //   }
+      self.moveElement.css('margin-top', '-' + (self.height * 3) + 'px')
     }
   }
 })()
