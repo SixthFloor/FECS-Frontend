748987d093d6747612f34d1de010c9e149937338
GuroKung <greenblood11@hotmail.com>
2015-12-07 23:01:52 +0700

    can filter price

--- src/js/controller/category.js
+++ src/js/controller/category.js
@@ -29,19 +29,21 @@
     self.sort_by = self.sortOptions[0].value
     self.category_name = $stateParams.category_name
 
-    //  API path
-    var url = ''
-    if (self.category_name === 'search') {
-      url = environment.getBaseAPI() + 'product/search?query=' + searchService.getSearchQuery()
-    }
-    else if (self.category_name !== 'all') {
-      url = environment.getBaseAPI() + 'category/product/' + self.category_name
-    }
-    else url = environment.getBaseAPI() + 'product/all'
+    self.init = function () {
+      //  API path
+      self.url = ''
+      if (self.category_name === 'search') {
+        self.url = environment.getBaseAPI() + 'product/search?query=' + searchService.getSearchQuery()
+      }
+      else if (self.category_name !== 'all') {
+        self.url = environment.getBaseAPI() + 'category/product/' + self.category_name
+      }
+      else self.url = environment.getBaseAPI() + 'product/all'
 
-    $http.get(url).success(function (response) {
-      self.productList = response
-    })
+      $http.get(self.url).success(function (response) {
+        self.productList = response
+      })
+    }
 
     self.viewProduct = function (id) {
       $state.transitionTo('product', {product_id: id})
@@ -61,5 +63,47 @@
       productService.clearProduct()
       $state.transitionTo('addproduct')
     }
+
+    self.clear = function () {
+      console.log('clear')
+      self.price = 0
+      self.init()
+    }
+
+    self.filterPrice = function () {
+      console.log('filter price')
+      $http.get(self.url).success(function (response) {
+        self.productList = response
+      }).then(function(response) {
+        var list = self.productList
+        var filter = []
+        for (var i = 0; i < list.length; i++) {
+          switch (self.price) {
+            case '1':
+              if(list[i].price < 1000) {
+                filter.push(list[i])
+              }
+            break
+            case '2':
+              if(list[i].price >= 1000 && list[i].price <= 5000) {
+                filter.push(list[i])
+              }
+            break
+            case '3':
+              if(list[i].price >= 5000 && list[i].price <= 10000) {
+                filter.push(list[i])
+              }
+            break
+            case '4':
+              if(list[i].price > 10000) {
+                filter.push(list[i])
+              }
+            break
+          }
+        }
+        self.productList = filter
+      })
+    }
+    self.init()
   }
 })()
