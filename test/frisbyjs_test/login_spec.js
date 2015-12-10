3f6350b05cbaf242659451facded662c725254dd
JL <world.jadi@gmail.com>
2015-12-11 01:56:11 +0700

    finish login spec

---
+++ test/frisbyjs_test/login_spec.js
@@ -0,0 +1,69 @@
+var frisby = require('frisby')
+
+frisby.create('should be responsed with user detail when login with corrected user')
+  .post('http://128.199.133.224/api/authentication/login', {
+      "email": "nara@gmail.com",
+      "password": "12345678"
+  },{json: true})
+  .expectJSONTypes({
+    "token": String,
+    "user": {
+      "id": Number,
+      "email": String,
+      "firstName": String,
+      "lastName": String,
+      "joiningDate": Number,
+      function(val) {
+        expect(val).toContainJsonTypes({
+          "address1": function(val) { expect(val).toBeTypeOrNull(String); }
+        })
+      },
+      function(val) {
+        expect(val).toContainJsonTypes({
+          "address2": function(val) { expect(val).toBeTypeOrNull(String); }
+        })
+      },
+      function(val) {
+        expect(val).toContainJsonTypes({
+          "province": function(val) { expect(val).toBeTypeOrNull(String); }
+        })
+      },
+      function(val) {
+        expect(val).toContainJsonTypes({
+          "zipcode": function(val) { expect(val).toBeTypeOrNull(String); }
+        })
+      },
+      function(val) {
+        expect(val).toContainJsonTypes({
+          "telephone_number": function(val) { expect(val).toBeTypeOrNull(String); }
+        })
+      },
+      function(val) {
+        expect(val).toContainJsonTypes({
+          "card_name": function(val) { expect(val).toBeTypeOrNull(String); }
+        })
+      },
+      function(val) {
+        expect(val).toContainJsonTypes({
+          "card_number": function(val) { expect(val).toBeTypeOrNull(String); }
+        })
+      },
+      function(val) {
+        expect(val).toContainJsonTypes({
+          "expirationDate": function(val) { expect(val).toBeTypeOrNull(Number); }
+        })
+      },
+    },
+  })
+.toss()
+
+frisby.create('should be responsed with internal server error when login with incorrect user')
+  .post('http://128.199.133.224/api/authentication/login', {
+      "email": "nara123@gmail.com",
+      "password": "12345678"
+  },{json: true}).
+  expectJSON(
+    {
+      "description": "Internal Server Error"
+    })
+  .toss()
