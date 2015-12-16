87139da6f4994632665d33f6049c2d8a73450514
GuroKung <greenblood11@hotmail.com>
2015-12-16 17:00:06 +0700

    mock value

--- src/js/controller/shipping.js
+++ src/js/controller/shipping.js
@@ -14,6 +14,37 @@
   ShippingController.$inject = ['$scope', '$http', '$state']
   function ShippingController ($scope, $http, $state) {
     var self = this
+    self.orderList = [{
+      orderNumber: 55,
+      orderDate: 1450258183000,
+      user:{
+        firstName: 'Guro',
+        lastName: 'GreenBlooD',
+        address: 'This is my home'
+      },
+      shipping: 1450228183000
+    },{
+      orderNumber: 55,
+      orderDate: 1450258183000,
+      user:{
+        firstName: 'Guro',
+        lastName: 'GreenBlooD',
+        address: 'This is my home'
+      },
+      shipping: 1450458183000
+    },{
+      orderNumber: 55,
+      orderDate: 1450258183000,
+      user:{
+        firstName: 'Guro',
+        lastName: 'GreenBlooD',
+        address: 'This is my home'
+      },
+      shipping: 1450378183000
+    }]
 
+    self.gotoView = function (orderNo) {
+      $state.transitionTo('vieworder', {orderNumber: orderNo})
+    }
   }
 })()
